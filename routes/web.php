400bc1cd810c617b9af2b74cd5284ef7ed44300f
andri <aarfianto11@gmail.com>
2020-05-29 12:02:17 +0700

    routes api spaces & space(browse)

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('space', 'SpaceController');
+Route::get('/browse/space', 'SpaceController@browse')->name('space.browse');
